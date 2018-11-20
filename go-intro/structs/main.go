b6a0fa1822b8a9c4e4087d95e0a3eaf4be9062e0
Derik Solis <derik98solis@gmail.com>
2018-11-20 08:54:22 -0500

    adding all of my golang practice

---
+++ go-intro/structs/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+	var alex person
+	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
+}
